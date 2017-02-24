473ec9f99d3c30ffb800905600556fc623fe72a8
Maitrey Patel <mp458.njit.edu>
2017-02-23 23:53:29 -0500

    second commit

--- category_list.php
+++ category_list.php
@@ -23,18 +23,52 @@ $statement->closeCursor();
 <header><h1>Product Manager</h1></header>
 <main>
     <h1>Category List</h1>
+    
     <table>
-        <tr>
-            <th>Name</th>
-            <th>&nbsp;</th>
-        </tr>
-        
-        <!-- add code for the rest of the table here -->
+      <tr>
+		<th>Name</th>
+		<th>&nbsp;</th>
+      </tr>
+	<th>
+	
+             <?php foreach ($categories as $category) : ?>
+	<tr>
+	<ul>
+	<td>
+	     <a href=".?category_id=<?php echo  $category['categoryID']; ?>">
+	    <?php echo $category['categoryName']; ?>
+                 </a>
+            
+     
+	</td>
+        <td> 
+   	
+	
+		<form action="delete_catergory.php" method="post">
+		<input type="hidden" name="category_id" value ="2">
+		<input type="submit" value="Delete">
+		</form>
+	
+	</tr>
+	</ul>
+	</td>
+	     <?php endforeach; ?>
+	
+	</th>
+
+ 
+  
+  <!-- add code for the rest of the table here -->
     
     </table>
 
     <h2>Add Category</h2>
-    
+    <form action="add_category.php" method="post"
+    id="add_category_form">
+    <label>Name</label>
+    <input type="text" name="name">
+    <input id="add_category_button" type="submit" value="Add">
+    </form>		
     <!-- add code for the form here -->
     
     <br>
